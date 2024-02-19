35575eeff4db829f544473b12e5c1d9c453a140c
nomansd1 <noman.sd114@gmail.com>
2024-02-19 12:45:53 +0500

    implement bcrypt password hashing and comparing password

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";    
+import jwt from "jsonwebtoken";
 
 const userSchema = new Schema(
     {
@@ -47,5 +49,18 @@ const userSchema = new Schema(
         timestamps: true
     }
 )
+
+// Password encryption
+userSchema.pre("save", async function(next) {
+    if (!this.isModified("password")) return next();
+    
+    this.password = bcrypt.hash(this.password, 10);
+    next();
+})
+
+// Checking password
+userSchema.methods.isPasswordCorrect = async function(password) {
+    return await bcrypt.compare(password, this.password);
+}
 
 export const User = mongoose.model("User", userSchema); 
--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
 
 const videoSchema = new Schema(
     {
@@ -39,3 +41,7 @@ const videoSchema = new Schema(
         timestamps: true,
     }
 )
+
+videoSchema.plugin(mongooseAggregatePaginate); 
+
+export const Video = mongoose.model("Video", videoSchema);
