ac5d647a33d496a623c23b4bdf1fda9571d43511
Noman Ali <noman.sd114@gmail.com>
2024-04-02 02:28:15 +0500

    completed logout and login functionality

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -166,8 +166,31 @@ const loginUser = asyncHandler(async (req, res) => {
 })
 
 const logoutUser = asyncHandler(async (req, res) => {
-    // Step 01 - Clear cookies
-    // Step 02 - Remove refresh token from DB 
+    // Step 01 - Remove refresh token from DB
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken: undefined
+            }
+        },
+        {
+            new: true
+        }
+    )
+    
+    // Step 02 - Clear cookies
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+        .status(200)
+        .clearCookie("accessToken", options)
+        .clearCookie("refreshToken", options)
+        .json( new ApiResponse(200, "User logged out") )
+
 })
 
-export { registerUser, loginUser };
+export { registerUser, loginUser, logoutUser };
